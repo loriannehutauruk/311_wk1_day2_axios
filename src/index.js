69c91cc24e0c6dd1a54ee39ffe2e8c1a5e5ec30a
loriannehutauruk <lori@hutauruk.com>
2019-07-10 20:53:11 -0500

    adding the week1day2 classwork with code written in class - awaiting update on tests

--- src/index.js
+++ src/index.js
@@ -1,24 +1,74 @@
 // import axios here
-
+const axios = require('axios')
+const config = require('../config')
+//console log is just to show that this would go into the config file and retrieve the api key.
+console.log(config.API_KEY)
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
-const api_key = 'd771b19ef336ed8381def3a60b574464'
+// API key was deleted
 
 const discoverMovie = () => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${config.API_KEY}`
   // code here
+  axios.get(url)
+    .then(function (response){
+      //handle success
+      // console.log(response.data.results);
+      return response.data.results
+    })
+    .catch(function (error) {
+      //handles errors
+      console.log(error);
+    })
+    .finally (function (){
+      //always executed. this is where you can define what you always want to run
+    });
 }
+discoverMovie();  
 
 const getMovieById = (id) => {
-  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
-  // code here
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${config.API_KEY}`
+  // code here. We are going into the url, created an object params and get the id. 
+  axios.get(url, {
+    params: {
+      id: 500
+    }
+  })
+  .then(function (response){
+    //handle success
+    // console.log(response.data.original_title);
+    return response.data.original_title
+  })
+  .catch(function (error) {
+    //handles errors
+    console.log(error);
+  })
+  .finally (function (){
+    //always executed. this is where you can define what you always want to run
+  }); 
 }
+getMovieById();
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
-  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
+  const fakeId = 5783 // FAKE ID HERE
+  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${config.API_KEY}`
   // code here
+  axios.get(url)
+  .then(function (response){
+    //handle success
+   console.log(response)
+  
+  })
+  .catch(function (error) {
+    //handles errors
+    // console.log(error);
+    return error
+  })
+  .finally (function (){
+    //always executed. this is where you can define what you always want to run
+  });
+
 }
 
 
